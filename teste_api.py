d3ba9158b36ffd25aabc22013e0c0e026abc479f
Luiz Fernando <verlyvieiral@gmail.com>
2026-08-31 19:15:26 -0300

    Adicionando a Média MOvel Simples

--- teste_api.py
+++ teste_api.py
@@ -23,6 +23,8 @@ candle = requests.get(f"https://api.binance.com/api/v3/klines?symbol={moeda}&int
 
 dado2 = candle.json()
 
+fechamento_s = []
+
 for candle in dado2:
     abertura = float(candle[1])
     maxima = float(candle[2])
@@ -30,9 +32,33 @@ for candle in dado2:
     fechamento = float(candle[4])
     volume = float(candle[5])
 
+    fechamento_s.append(fechamento)
+
     print(f'Abertura:   R$ {abertura:,.2f}')
     print(f'Máxima:   R$ {maxima:,.2f}')
     print(f'Mínima:   R$ {minima:,.2f}')
     print(f'Fechamento:   R$ {fechamento:,.2f}')
     print(f'Volume:   {volume:,.2f} {moeda}')
     print(f'-'*40)
+
+#SMA
+
+periodo = int(input('Coloque o periodo para calcular a Média Móvel Simples(SMA): '))
+
+if len(fechamento_s) >= periodo:
+    soma = sum(fechamento_s[-periodo:])
+    sma = soma/periodo
+
+    print(f'SMA {periodo}: R${sma:,.2f}\n')
+
+else:
+    print('Não há candle suficiente para clacuklar a SMA.\n')
+
+if preco > sma:
+    print(f'A cotação atual está  em R$ {(sma - preco):.2f} acima do SMA. \n')
+
+elif preco < sma:
+    print(f'A cotação atual está  em R$ {(sma - preco):.2f} abaixo do SMA\n')
+
+else:
+    print(f'A cotação está igual a SMA.\n')
